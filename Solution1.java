c61211fe9c6d1a540fddcbdb8c0e9d1ddd128eaf
Sanskar Mittal <mttlsanskar3445@gmail.com>
2026-05-28 11:29:42 +0530

    Added Java solution

---
+++ Solution1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int myInt1 = scanner.nextInt();
+       int myInt2 = scanner.nextInt();
+       int myInt3 = scanner.nextInt();
+      
+       scanner.close();
+       
+       System.out.println("" + myInt1);
+        System.out.println("" + myInt2);
+        System.out.println("" + myInt3);
+       
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
